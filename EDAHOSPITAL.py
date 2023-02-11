703ca27927fc9a92fb667aff88fb2178b6a39f1a
30Prashantrai <102423024+30Prashantrai@users.noreply.github.com>
2023-02-11 17:31:19 +0530

    Update EDAHOSPITAL.py

--- EDAHOSPITAL.py
+++ EDAHOSPITAL.py
@@ -1,17 +1,9 @@
-import os
+import pandas as pd
+from pandas_profiling import ProfileReport
 
-print(os.getcwd())
-
-
-# import pandas as pd
-# from pandas_profiling import ProfileReport
-#
-# df = pd.read_excel("Hospital.xlsx")
-# print(df)
-#
-# profile = ProfileReport(df)
-# profile.to_file(output_file="Hospital.pdf")
-
-
-
-
+#reading hospital data 
+df = pd.read_excel("Hospital.xlsx")
+print(df)
+#creating profile report in html format
+profile = ProfileReport(df)
+profile.to_file(output_file="Hospital.html")
