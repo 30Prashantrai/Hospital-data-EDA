408d576d4f76775a475706099ca06be2fd52e358
30Prashantrai <102423024+30Prashantrai@users.noreply.github.com>
2023-02-11 17:28:04 +0530

    Add files via upload

---
+++ EDAHOSPITAL.py
@@ -0,0 +1,17 @@
+import os
+
+print(os.getcwd())
+
+
+# import pandas as pd
+# from pandas_profiling import ProfileReport
+#
+# df = pd.read_excel("Hospital.xlsx")
+# print(df)
+#
+# profile = ProfileReport(df)
+# profile.to_file(output_file="Hospital.pdf")
+
+
+
+
